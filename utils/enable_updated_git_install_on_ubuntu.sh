415be9f8c798cdf3cc12e327908e77812bed6145
Bari J. Ballew <bari.ballew@gmail.com>
2022-02-05 22:16:27 -0500

    docs: describe why you need the Git package archive

--- utils/enable_updated_git_install_on_ubuntu.sh
+++ utils/enable_updated_git_install_on_ubuntu.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+# ubuntu's package repo doesn't have up-to-date git
+# this repository is maintained by the Git team
+# add it so you can use modern git!
 sudo add-apt-repository ppa:git-core/ppa
