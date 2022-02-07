78c23dafa467f2c3e0fb5eeb7552178422228d37
Bari J. Ballew <bari.ballew@gmail.com>
2022-02-06 21:20:30 -0500

    feat: add convenience script to copy files into home

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cp -R global_configs/.bash_profile \
+      global_configs/.condarc \
+      global_configs/.gitconfig \
+      global_configs/.gitignore \
+      global_configs/.vim/ \
+      global_configs/.vimrc \
+      $HOME
