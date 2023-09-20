a16e166b1f010a125d9bfd945358ffe5f03ae3c6
Bari J. Ballew <bari.ballew@gmail.com>
2023-09-19 22:33:54 -0400

    feat: add aws config to access ec2 permissions via a profile

--- setup.sh
+++ setup.sh
@@ -7,4 +7,7 @@ cp -R global_configs/.bash_profile \
       global_configs/.vimrc \
       $HOME
 
+mkdir -p $HOME/.aws
+cp global_configs/.aws/config $HOME/.aws/config
+
 bash $PWD/utils/get_mambaforge.sh
